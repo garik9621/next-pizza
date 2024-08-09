651493493c1592b771d823cc0e5ece18c5d3e721
Igor Seliverstov <igrseliwjorstow@bk.ru>
2024-08-09 09:04:39 +0300

    Initial commit

---
+++ components/ui/index.ts
@@ -0,0 +1,8 @@
+export { Button } from './button';
+export { Checkbox } from './checkbox';
+export { Select } from './select';
+export { Skeleton } from './skeleton';
+export { Drawer } from './drawer';
+export { Slider } from './slider';
+export { Input } from './input';
+export { Popover } from './popover';
