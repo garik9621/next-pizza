f3fd9cc4f9ef874848976c2100e3c6eabc3a1e26
Igor Seliverstov <igrseliwjorstow@bk.ru>
2024-08-22 10:47:28 +0300

    Сайдбра с фильтрами, категории под шапкой

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
-import Image from "next/image";
-import {Button} from "@/components/ui/button";
+import {Container, Filters, Title, TopBar} from "@/components/shared";
 
 export default function Home() {
-  return (
-    <h1>Home</h1>
+  return <>
+    <Container className="mt-10">
+      <Title text="Все пиццы" size="lg" className="font-extrabold"/>
+    </Container>
 
-  );
+    <TopBar/>
+
+    <Container className="mt-10 pb-14">
+      <div className="flex gap-[60px]">
+        <div className="w-[250px]">
+          <Filters />
+        </div>
+        <div className="flex-1">
+          <div className="flex flex-col gap-16">
+
+          </div>
+        </div>
+      </div>
+    </Container>
+  </>;
 }
